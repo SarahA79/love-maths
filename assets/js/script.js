15e0c9acde266fdda7a290172c13c5b626c89091
Sarah Ankers <mumspride@hotmail.co.uk>
2024-08-05 14:49:09 +0000

    create event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,52 @@
-function runGame(){
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+                console.log("submit was clicked");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+                console.log("you clicked a button that wasnt submit");
+            }
+        });
+
+    }
+});
+
+function runGame() {
 
 }
-function checkAnswer(){
 
-}
-function calculateCorrectAnswer(){
-
-} 
-
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementScore() {
 
 }
 
-function displaySubtractQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
-    
+function displayAdditionQuestion() {
+
 }
 
-function displayDivideQuestion(){
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
     
 }
