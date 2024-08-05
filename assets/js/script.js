6e84f1fc2f00f4a29a390ce4db428e74721c8441
Sarah Ankers <mumspride@hotmail.co.uk>
2024-08-05 16:03:58 +0000

    create addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,35 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-                console.log("submit was clicked");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-                console.log("you clicked a button that wasnt submit");
+                runGame(gameType);
             }
         });
-
     }
-});
-/**
- * The main game "loop" is called when the script is first loaded 
- * and after the users answer has been submitted.
- */
-function runGame() {
-//creates two random numbers between 1 and 25.
-let num1 = Math.flooor(Math.random() * 25)+1;
-let num2 = Math.flooor(Math.random() * 25)+1;
-}
 
+    runGame("addition");
+
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
+}
 
 function checkAnswer() {
 
@@ -45,8 +54,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
